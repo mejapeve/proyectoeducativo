2253ed823efcd49c89226d82e490986d18134324
cristian <cristianjojoa01@gmail.com>
2020-01-19 18:21:07 -0500

    test login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,25 +57,21 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
-                        @guest
+                        @guest('afiliadoempresa')
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('employee.login') }}">{{ __('Login') }}</a>
+                                <a class="nav-link" href="{{ route('employee.login') }}">{{ __('Employee Login') }}</a>
                             </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                    {{ Auth::guard('afiliadoempresa')->user()->employee_id }}
+                                    <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('employee.logout') }}"
                                        onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                 document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
