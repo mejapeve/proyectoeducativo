14d1342dad5e61f104e6924fb71ae1d90c6692a2
cristian <cristianjojoa01@gmail.com>
2020-02-29 16:10:44 -0500

    vendor email

--- resources/views/vendor/mail/html/user.blade.php
+++ resources/views/vendor/mail/html/user.blade.php
@@ -1,1 +1,28 @@
-<?php
+<div hidden>
+
+
+    {!! html_entity_decode('<h3></h3>') !!}
+
+</div>
+<table class="action" align="" width="100%" cellpadding="0" cellspacing="0" role="presentation">
+    <tr>
+        <td align="center">
+            <table width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation">
+                <tr>
+                    <td align="">
+                        <table border="0" cellpadding="0" cellspacing="0" role="presentation">
+                            <tr>
+                                <td>
+                                    <h3>Usuraio: {{$user->user_name}}</h3>
+                                    @foreach($user->affiliated_company as $company_rol)
+                                        <h4>empresa: {{$company_rol->company->name}} rol: {{$company_rol->rol->name}}</h4>
+                                    @endforeach
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
