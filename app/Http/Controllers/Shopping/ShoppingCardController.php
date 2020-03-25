972554964f73d3f0837b1ccca72f416f222c9004
cristian <cristianjojoa01@gmail.com>
2020-03-24 19:17:37 -0500

    servicio registrar comprar carrito de compras

--- app/Http/Controllers/Shopping/ShoppingCardController.php
+++ app/Http/Controllers/Shopping/ShoppingCardController.php
@@ -42,8 +42,31 @@ class ShoppingCardController extends Controller
 
         }
 
-        return $shopingCarts;
+        return response()->json(['data'=>$shopingCarts],200);
+    }
+
+    public function create(Request $request){
+
+        $data = $request->all();
+        $shopingCart = new ShopingCart();
+        $shopingCart->company_affiliadted_id = $data['company_affiliadted_id'];
+        $shopingCart->session_id = $data['session_id'];
+        $shopingCart->rating_plan_id = $data['rating_plan_id'];
+        if(isset($data['kit_id']))
+            $shopingCart->kit_id = $data['kit_id'];
+        if(isset($data['payment_status']))
+            $shopingCart->payment_status = intval($data['payment_status']);
+        if(isset($data['payment_transaction_id']))
+            $shopingCart->payment_transaction_id = intval($data['payment_transaction_id']);
+        if(isset($data['payment_init_date']))
+            $shopingCart->payment_init_date = intval($data['payment_init_date']);
+        if(isset($data['shipping_price']))
+            $shopingCart->shipping_price = intval($data['shipping_price']);
+        if(isset($data['shipping_price']))
+            $shopingCart->shipping_price = intval($data['shipping_price']);
+        $shopingCart->save();
+
+        return response()->json(['data'=>$shopingCart,'messagge'=> 'se ha registrado el producto correctamente'],200);
 
     }
-	
 }
