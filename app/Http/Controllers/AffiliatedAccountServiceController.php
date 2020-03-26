c7939ffe17bc498b830ab93de171316ea3a00245
cristian <cristianjojoa01@gmail.com>
2020-03-26 16:44:24 -0500

    cache redis con laravel

--- app/Http/Controllers/AffiliatedAccountServiceController.php
+++ app/Http/Controllers/AffiliatedAccountServiceController.php
@@ -5,18 +5,20 @@ namespace App\Http\Controllers;
 use App\Models\AffiliatedAccountService;
 use Illuminate\Http\Request;
 use App\Traits\RelationRatingPlan;
+use Illuminate\Support\Facades\DB;
+
 class AffiliatedAccountServiceController extends Controller
 {
     //
     use RelationRatingPlan;
     public function get (Request $request,$affiliatedId){
-
-        $affiliatedAccountService = AffiliatedAccountService::
-            with('rating_plan')
-            ->where('company_affiliated_id',$affiliatedId)->get();
+        //cache()->tags('connection_sequences')->flush();
+        //DB::connection()->enableQueryLog();
+        $affiliatedAccountService = AffiliatedAccountService::where('company_affiliated_id',$affiliatedId)->
+            with('rating_plan')->get();
         $affiliatedAccountService = $this->relation_rating_plan($affiliatedAccountService);
 
-
+        //return DB::getQueryLog();//$shopingCarts;
         return $affiliatedAccountService;
     }
 
--- app/Traits/RelationRatingPlan.php
+++ app/Traits/RelationRatingPlan.php
@@ -10,20 +10,28 @@ trait RelationRatingPlan
 {
     //
     public function relation_rating_plan ($shopingCarts){
-
+        $sequencesCache = cache()->tags('connection_sequences')->rememberForever('sequences',function(){
+           return CompanySequence::all();
+        });
+        $momentsCache = cache()->tags('connection_moments')->rememberForever('moments',function(){
+            return SequenceMoment::all();
+        });
+        $experiencesCache = cache()->tags('connection_experiences')->rememberForever('experiences',function(){
+            return MomentExperience::all();
+        });
         for ($i=0; $i < count($shopingCarts); $i++) {
             if (isset($shopingCarts[$i]->rating_plan)) {
                 if ($shopingCarts[$i]->rating_plan->sequences_included !== null) {
                     $sequences = explode(',', $shopingCarts[$i]->rating_plan->sequence_company_ids);
-                    $shopingCarts[$i]['rating_plan']['sequences'] = CompanySequence::whereIn('id', $sequences)->get();
+                    $shopingCarts[$i]['rating_plan']['sequences'] = $sequencesCache->whereIn('id', $sequences);
                 } else {
                     if ($shopingCarts[$i]->rating_plan->moments_included !== null) {
-                        $moments = explode(',', $shopingCarts[$i]->rating_plan->sequence_moments_ids);
-                        $shopingCarts[$i]['rating_plan']['moments'] = SequenceMoment::whereIn('id', $moments)->get();
+                        $moments = explode(',', $shopingCarts[$i]->rating_plan->sequence_moment_ids);
+                        $shopingCarts[$i]['rating_plan']['moments'] = $momentsCache->whereIn('id', $moments);
                     } else {
                         if ($shopingCarts[$i]->rating_plan->experiences_included !== null) {
                             $experiences = explode(',', $shopingCarts[$i]->rating_plan->sequence_experience_ids);
-                            $shopingCarts[$i]['rating_plan']['experiences'] = MomentExperience::whereIn('id', $experiences)->get();
+                            $shopingCarts[$i]['rating_plan']['experiences'] = $experiencesCache->whereIn('id', $experiences);
                         }
 
                     }
@@ -31,6 +39,31 @@ trait RelationRatingPlan
             }
         }
         return $shopingCarts;
+/*
+        $CompanySequence=CompanySequence::all();
+        $SequenceMoment=SequenceMoment::all();
+        $MomentExperience=MomentExperience::all();
+        for ($i=0; $i < count($shopingCarts); $i++) {
+            if (isset($shopingCarts[$i]->rating_plan)) {
+                if ($shopingCarts[$i]->rating_plan->sequences_included !== null) {
+                    $sequences = explode(',', $shopingCarts[$i]->rating_plan->sequence_company_ids);
+                    $shopingCarts[$i]['rating_plan']['sequences'] = $CompanySequence->whereIn('id', $sequences);
+                } else {
+                    if ($shopingCarts[$i]->rating_plan->moments_included !== null) {
+                        $moments = explode(',', $shopingCarts[$i]->rating_plan->sequence_moments_ids);
+                        $shopingCarts[$i]['rating_plan']['moments'] = $SequenceMoment->whereIn('id', $moments);
+                    } else {
+                        if ($shopingCarts[$i]->rating_plan->experiences_included !== null) {
+                            $experiences = explode(',', $shopingCarts[$i]->rating_plan->sequence_experience_ids);
+                            $shopingCarts[$i]['rating_plan']['experiences'] = $MomentExperience->whereIn('id', $experiences);
+                        }
+
+                    }
+                }
+            }
+        }
+        return $shopingCarts;
+*/
     }
 
 
