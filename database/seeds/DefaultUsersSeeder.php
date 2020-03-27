fddd136bec790ad7ed54c223057050b5ba03453a
cristian <cristianjojoa01@gmail.com>
2020-03-27 12:10:35 -0500

    usuarios estratégica roles padre y administrador conexiones

--- database/seeds/DefaultUsersSeeder.php
+++ database/seeds/DefaultUsersSeeder.php
@@ -14,9 +14,14 @@ class DefaultUsersSeeder extends Seeder
         //
 
         $users = [
-            ['user_name'=>'CristianJojoa','nombre'=>'Cristian','apellido'=>'Jojoa','correo'=>'cristianjojoa01@gmail.com','password'=>'cristianjojoa01','empresa_id'=>1],
-            ['user_name'=>'HenryGarzon','nombre'=>'Henry ','apellido'=>'Garzon','correo'=>'hsgarzon2020@gmail.com','password'=>'hsgarzon2020','empresa_id'=>1],
-            ['user_name'=>'DavidCamacho','nombre'=>'David','apellido'=>'Camacho','correo'=>'davithc01@gmail.com','password'=>'davithc01','empresa_id'=>1,]
+            ['rol_id'=>[1,4],'user_name'=>'CristianJojoa','nombre'=>'Cristian','apellido'=>'Jojoa','correo'=>'cristianjojoa01@gmail.com','password'=>'cristianjojoa01','empresa_id'=>1],
+            ['rol_id'=>[1,4],'user_name'=>'HenryGarzon','nombre'=>'Henry ','apellido'=>'Garzon','correo'=>'hsgarzon2020@gmail.com','password'=>'hsgarzon2020','empresa_id'=>1],
+            ['rol_id'=>[1,4],'user_name'=>'DavidCamacho','nombre'=>'David','apellido'=>'Camacho','correo'=>'davithc01@gmail.com','password'=>'davithc01','empresa_id'=>1,],
+            ['rol_id'=>[3,4],'user_name'=>'JairoContreras','nombre'=>'Jairo','apellido'=>'Contreras','correo'=>'jairocontrerasch@gmail.com','password'=>'jairocontrerasch','empresa_id'=>1,],
+            ['rol_id'=>[3,4],'user_name'=>'JordyChimbí','nombre'=>'Jordy','apellido'=>'Chimbí','correo'=>'jordychimbi@gmail.com','password'=>'jordychimbi','empresa_id'=>1,],
+            ['rol_id'=>[3,4],'user_name'=>'CatalinaAlbeláez','nombre'=>'Catalina','apellido'=>'Albeláez','correo'=>'luciacatalina@gmail.com','password'=>'luciacatalina','empresa_id'=>1,],
+            ['rol_id'=>[3,4],'user_name'=>'YessicaPinzón','nombre'=>'Yessica','apellido'=>'Pinzón','correo'=>'jesspinzong@gmail.com','password'=>'jesspinzong','empresa_id'=>1,],
+
         ];
 
         foreach ($users as $user){
@@ -28,25 +33,23 @@ class DefaultUsersSeeder extends Seeder
             $userN->email = $user['correo'];
             $userN->password = \Illuminate\Support\Facades\Hash::make($user['password']);
             $userN->save();
-/*
-            $affiliatedCompany = new \App\Models\CompaniesAffiliated();
-            $affiliatedCompany->affiliated_id = $userN->id;
-            $affiliatedCompany->company_id = $user['empresa_id'];
-            $affiliatedCompany->save();
-*/
-            $affiliatedCompanyRoles = new \App\Models\AffiliatedCompanyRole();
-            $affiliatedCompanyRoles->affiliated_company_id = $userN->id;
-            $affiliatedCompanyRoles->rol_id = 1;
-            $affiliatedCompanyRoles->company_id = $user['empresa_id'];
-            $affiliatedCompanyRoles->save();
+            foreach ($user['rol_id'] as $rol){
+                $affiliatedCompanyRoles = new \App\Models\AffiliatedCompanyRole();
+                $affiliatedCompanyRoles->affiliated_company_id = $userN->id;
+                $affiliatedCompanyRoles->rol_id = $rol;
+                $affiliatedCompanyRoles->company_id = $user['empresa_id'];
+                $affiliatedCompanyRoles->save();
 
+            }
+
+/*
             $affiliatedCompanyRoles = new \App\Models\AffiliatedCompanyRole();
             $affiliatedCompanyRoles->affiliated_company_id = $userN->id;
             $affiliatedCompanyRoles->rol_id = 4;
             $affiliatedCompanyRoles->company_id = $user['empresa_id'];
             $affiliatedCompanyRoles->save();
 
-
+*/
         }
     }
 }
