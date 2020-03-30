26b7651af5a757f9e54048910abe55dea436fda2
cristian <cristianjojoa01@gmail.com>
2020-03-29 20:13:30 -0500

    ajuste Clase shopping_cart

--- database/migrations/2020_03_11_144339_create_shopping_carts_table.php
+++ database/migrations/2020_03_11_144339_create_shopping_carts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateShopingCartsTable extends Migration
+class CreateShoppingCartsTable extends Migration
 {
     /**
      * Run the migrations.
