c03ae4a5678cea0512a3a2e9cb849b3861175ddf
cristian <cristianjojoa01@gmail.com>
2020-03-31 22:01:33 -0500

    comentar kit elements factory

--- database/seeds/ElementSeeder.php
+++ database/seeds/ElementSeeder.php
@@ -12,9 +12,9 @@ class ElementSeeder extends Seeder
     public function run()
     {
         //
-        factory(\App\Models\Element::class,50)->create();
-        factory(\App\Models\Kit::class,3)->create();
-        factory(\App\Models\KitElement::class,20)->create();
+        //factory(\App\Models\Element::class,50)->create();
+        //factory(\App\Models\Kit::class,3)->create();
+       // factory(\App\Models\KitElement::class,20)->create();
 
     }
 }
