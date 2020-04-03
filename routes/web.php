162b439b188ef80fea4aa6c2e580da735ab02111
hsgarzon2020 <hsgarzon2020@gmail.com>
2020-04-02 23:11:00 -0500

    add checkout route
    prueba 2 de checkout mercadoPago

--- routes/web.php
+++ routes/web.php
@@ -97,6 +97,7 @@ Route::group(['middleware' =>['auth:afiliadoempresa', 'companyaffiliated', 'comp
 Route::group([],function (){
         Route::get('shoppingcard', ['as' => 'shoppingcard', 'uses' => 'Shopping\ShoppingCartController@index']);
         Route::get('get_shopping_cart/{user}', 'Shopping\ShoppingCartController@get_shopping_cart')->name('get_shopping_cart');//->middleware('auth:afiliadoempresa');
+        Route::get('checkout', ['as' => 'checkout', 'uses' => 'Shopping\CheckoutController@index']);
     }
 );
 
