7a406088563f611f100a1bb290d3a35da6191313
hsgarzon2020 <hsgarzon2020@gmail.com>
2020-04-04 10:48:45 -0500

    checkout mercadoPago
    Test 4 para integración con mercadoPago (Ventana modal)

--- app/Http/Controllers/Shopping/CheckoutController.php
+++ app/Http/Controllers/Shopping/CheckoutController.php
@@ -3,9 +3,7 @@
 namespace App\Http\Controllers\Shopping;
 
 use App\Http\Controllers\Controller;
-use MercadoPago\SDK;
-use MercadoPago\Item;
-use MercadoPago\Preference;
+use MercadoPago;
 
 class CheckoutController extends Controller
 {
@@ -21,28 +19,33 @@ class CheckoutController extends Controller
     ]);
     }*/
 
-    public function __construct()
+    /*public function __construct()
     {
-        SDK::setClientId(
-            config("MERCADOPAGO_KEY")
-        );
-        SDK::setClientSecret(
-            config("MERCADOPAGO_SECRET")
-        );
-    }
+    MercadoPago\SDK::setClientId(
+    config("MERCADOPAGO_KEY")
+    );
+    MercadoPago\SDK::setClientSecret(
+    config("MERCADOPAGO_SECRET")
+    );
+    }*/
 
     public function index()
     {
-        SDK::setAccessToken('TEST-7394833091802936-031118-6efb7b3446ef18d20bccb024638e38f3-271000387');
-        $preference = new Preference();
+        //MercadoPago\SDK::setClientId("TEST-7b92f740-e376-40ee-8108-a8a0c3fa067a");
+        //MercadoPago\SDK::setClientSecret("TEST-7394833091802936-031118-6efb7b3446ef18d20bccb024638e38f3-271000387");
+        MercadoPago\SDK::setAccessToken('TEST-7394833091802936-031118-6efb7b3446ef18d20bccb024638e38f3-271000387');
+        # Create a preference object
+        $preference = new MercadoPago\Preference();
         // Crea un ítem en la preferencia
-        $item = new Item();
-        $item->title = 'Secuencia de prueba';
+        $item = new MercadoPago\Item();
+        //dd(MercadoPago\SDK::config());
+        $item->title = 'Yotopo y los astroamigos';
         $item->quantity = 1;
-        $item->unit_price = 75.56;
+        $item->unit_price = 5000;
         $preference->items = array($item);
-        $preference->save();    
-        return view('shopping.checkout')->with([$preference]);
+        $preference->save();
+        //dd($preference);
+        return view('shopping.checkout')->with("preference",$preference);
     }
 
 }
--- resources/views/shopping/checkout.blade.php
+++ resources/views/shopping/checkout.blade.php
@@ -1,14 +1,14 @@
-
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
     <form action="/procesar-pago" method="POST">
-        <script
-        src="https://www.mercadopago.com.co/integrations/v1/web-payment-checkout.js"
-        data-preference-id="<?php echo $preference->id; ?>">
-        </script>
-        @csrf
+       
     </form>
-</div>
+@endsection
+@section('js')
+        <script
+            src="https://www.mercadopago.com.co/integrations/v1/web-payment-checkout.js"
+            data-preference-id={{$preference->id}}
+        >
+        </script>
 @endsection
