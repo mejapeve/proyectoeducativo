cd1c1bc34ef8eba0fea4ebd0db040cb1af591276
cristian <cristianjojoa01@gmail.com>
2020-04-19 14:45:53 -0500

    ajuste migración

--- database/migrations/2020_03_25_111918_create_affiliated_account_services_table.php
+++ database/migrations/2020_03_25_111918_create_affiliated_account_services_table.php
@@ -19,10 +19,10 @@ class CreateAffiliatedAccountServicesTable extends Migration
             $table->foreign('company_affiliated_id')->references('id')->on('afiliado_empresas');
             $table->bigInteger('rating_plan_id')->unsigned()->nullable();
             $table->foreign('rating_plan_id')->references('id')->on('rating_plans');
-			$table->integer('rating_plan_type')->nullable();
-			$table->foreign('rating_plan_id')->references('id')->on('types_rating_plans');
+			$table->bigInteger('rating_plan_type')->unsigned()->nullable();
+			$table->foreign('rating_plan_type')->references('id')->on('types_rating_plans');
 			$table->bigInteger('shopping_cart_id')->unsigned()->	nullable();
-			$table->foreign('shopping_cart_id')->references('id')->on('shopping_cart_products');
+			$table->foreign('shopping_cart_id')->references('id')->on('shopping_carts');
             $table->date('init_date')->nullable();
             $table->date('end_date')->nullable();
             $table->timestamps();
