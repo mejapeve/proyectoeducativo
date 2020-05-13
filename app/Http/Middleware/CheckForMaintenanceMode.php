4aa0253392994aa7f3cd45082d3a2a60a545024b
cristian <cristianjojoa01@gmail.com>
2020-05-12 20:48:17 -0500

    prueba restringir ips para servidor de producción

--- app/Http/Middleware/CheckForMaintenanceMode.php
+++ app/Http/Middleware/CheckForMaintenanceMode.php
@@ -2,16 +2,70 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode as Middleware;
+use Closure;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
-class CheckForMaintenanceMode extends Middleware
+class CheckForMaintenanceMode
 {
+
     /**
-     * The URIs that should be reachable while maintenance mode is enabled.
+     * The application implementation.
      *
-     * @var array
+     * @var \Illuminate\Contracts\Foundation\Application
      */
-    protected $except = [
-        //
-    ];
+    protected $app;
+    protected $ips =
+        [
+            '172.21.0.1',
+            '244.244.244.244'
+
+        ];
+    protected $except =
+        [
+            'info/politicas-de-privacidad',
+            'info/terminos-y-condiciones'
+        ];
+    /**
+     * Create a new middleware instance.
+     *
+     * @param \Illuminate\Contracts\Foundation\Application $app
+     * @return void
+     */
+    public function __construct(Application $app)
+    {
+        $this->app = $app;
+    }
+
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+
+        if ($this->app->isDownForMaintenance() && !in_array($request->getClientIp(), $this->ips))
+        {
+            //dd($request->getClientIp(), $this->ips);
+            //Reviso si esta en la excepcion
+            foreach ($this->except as $except) {
+                if ($except !== '/') {
+                    $except = trim($except, '/');
+                }
+
+                if ($request->is($except)) {
+                    return $next($request);
+                }
+
+            }
+            return response()->view('errors.503');
+        }
+
+        return $next($request);
+    }
 }
