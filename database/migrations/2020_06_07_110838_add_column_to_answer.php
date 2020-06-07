eb19b21309d717f7bd71ffd47e420bd4c0181033
cristian <cristianjojoa01@gmail.com>
2020-06-07 11:27:13 -0500

    ajuste migraciones

---
+++ database/migrations/2020_06_07_110838_add_column_to_answer.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnToAnswer extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('answers', function (Blueprint $table) {
+            //
+            $table->string('concept')->nullable()->after('date_evaluation');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('answers', function (Blueprint $table) {
+            //
+        });
+    }
+}
