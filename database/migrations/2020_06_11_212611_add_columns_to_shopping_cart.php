d0e13ffea6b6b358f40ed3c360b18d892c67cf7c
cristian <cristianjojoa01@gmail.com>
2020-06-15 10:36:52 -0500

    ajuste migración

--- database/migrations/2020_06_11_212611_add_columns_to_shopping_cart.php
+++ database/migrations/2020_06_11_212611_add_columns_to_shopping_cart.php
@@ -15,14 +15,14 @@ class AddColumnsToShoppingCart extends Migration
     {
         Schema::table('shopping_carts', function (Blueprint $table) {
             //
-            $table->string('approval_code',15);
-            $table->string('payment_method',3)->comment('valor para registrar el método de pago:PSE:Tarjeta débito,TC:Tarjeta crédito');
+            $table->string('approval_code',15)->nullable()->after('payment_process_date');
+            $table->string('payment_method',3)->nullable()->after('approval_code')->comment('valor para registrar el método de pago:PSE:Tarjeta débito,TC:Tarjeta crédito');
         });
 
         Schema::table('questions', function (Blueprint $table) {
             //
-            $table->string('url_image')->nullable();
-            $table->string('url_video')->nullable();
+            $table->string('url_image')->nullable()->after('concept');
+            $table->string('url_video')->nullable()->after('url_image');
         });
     }
 
