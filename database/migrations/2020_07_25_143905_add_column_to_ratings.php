6a89357df7b237c8a87ef5a5fd7410ee6e4dc842
cristian <cristianjojoa01@gmail.com>
2020-07-25 14:43:06 -0500

    ajuste migración calificaciones

---
+++ database/migrations/2020_07_25_143905_add_column_to_ratings.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnToRatings extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('ratings', function (Blueprint $table) {
+            //
+            $table->string('color',15)->after('letter')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('ratings', function (Blueprint $table) {
+            //
+        });
+    }
+}
